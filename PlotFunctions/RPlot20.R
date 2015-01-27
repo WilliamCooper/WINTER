03a5c05e90794cf57dfdb8f108eab916b6f1240b
WilliamCooper <cooperw@ucar.edu>
2015-01-27 05:58:47 -0700

    Added additional plots for UHSAS, RSTB, VCSEL.

--- PlotFunctions/RPlot20.R
+++ PlotFunctions/RPlot20.R
@@ -5,14 +5,18 @@ RPlot20 <- function (data) {
   netCDFfile = open.ncdf(fname)
   CCDP <- get.var.ncdf(netCDFfile, "CCDP_LPC")
   CFSSP <- get.var.ncdf (netCDFfile, "CS100_LPT")
+  Time <- get.var.ncdf (netCDFfile, "Time")
+  time_units <- att.get.ncdf (netCDFfile, "Time", "units")
+  tref <- sub ('seconds since ', '', time_units$value)
+  Time <- as.POSIXct(as.POSIXct(tref, tz='UTC')+Time, tz='UTC')
   CellSizes <- att.get.ncdf (netCDFfile, "CCDP_LPC", "CellSizes")
   CellLimitsD <- CellSizes$value
   CellSizes <- att.get.ncdf (netCDFfile, "CS100_LPT", "CellSizes")
   CellLimitsF <- CellSizes$value
   layout(matrix(1:6, ncol = 2), widths = c(5,5), heights = c(5,5,6))
   op <- par (mar=c(2,2,1,1)+0.1)
-  for (j in 1:length(Data$Time)) {
-    if (is.na(Data$Time[j])) {next}
+  for (j in 1:length(Time)) {
+    if (is.na(Time[j])) {next}
     if (kount >= 24) {next}
     CDP <- CCDP[,j]
     FSSP <- CFSSP[,j]
@@ -25,7 +29,7 @@ RPlot20 <- function (data) {
       plot (CellLimitsD, CDP, type='s', ylim=c(1.e-1,1.e3), 
             xlab="Diameter [um]", log="y", col='blue', lwd=2)
       points (CellLimitsF, FSSP, type='s', col='darkgreen')
-      title(sprintf("Time=%s", strftime (Data$Time[j], format="%H:%M:%S", tz='UTC')), 
+      title(sprintf("Time=%s", strftime (Time[j], format="%H:%M:%S", tz='UTC')), 
             cex.main=.75)
       legend ("topright", legend=c("CDP", "FSSP"), col=c('blue', 'darkgreen'), 
               lwd=c(2,1), cex=0.75) 
